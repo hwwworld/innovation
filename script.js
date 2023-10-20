c4a65a1abb26f928a55809005605bf0c325fab8e
Hashan Wickramasingha Wadanambi <hashan.wadanambi@gmail.com>
2023-10-20 18:18:10 +0300

    jj

--- script.js
+++ script.js
@@ -2,35 +2,71 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
     const startRecording = document.getElementById('startRecording');
-    const stopRecording = document.getElementById('stopRecording');
+    const generatePDF = document.getElementById('generatePDF');
     const output = document.getElementById('output');
+    const pdfContainer = document.getElementById('pdfContainer');
+    
+    let convertedText = '';
 
     recognition.continuous = true; // Continuous listening
 
     recognition.onresult = function(event) {
         const last = event.results.length - 1;
         const text = event.results[last][0].transcript;
-
-        output.textContent = text;
+        
+        convertedText += text;
+        output.textContent = convertedText;
     };
 
     recognition.onstart = function() {
         startRecording.textContent = 'Recording...';
-        stopRecording.disabled = false;
+        generatePDF.disabled = true;
     };
 
     recognition.onend = function() {
         startRecording.textContent = 'Start Recording';
-        stopRecording.disabled = true;
+        generatePDF.disabled = false;
     };
 
     startRecording.addEventListener('click', function() {
         recognition.start();
     });
 
-    stopRecording.addEventListener('click', function() {
-        recognition.stop();
+    generatePDF.addEventListener('click', function() {
+        generateFormattedPDF(convertedText);
     });
+
+    function generateFormattedPDF(text) {
+        // You can use a PDF generation library like pdf-lib to create a PDF from the text.
+        // This is a simplified example, and you may need to configure the PDF layout and styling as needed.
+        const { PDFDocument, rgb } = PDFLib;
+
+        const pdfDoc = await PDFDocument.create();
+        const page = pdfDoc.addPage([400, 600]);
+        const { width, height } = page.getSize();
+        const contentStream = pdfDoc.register(
+          page.drawText(text, {
+            x: 10,
+            y: height - 30,
+            size: 12,
+            color: rgb(0, 0, 0),
+          })
+        );
+
+        page.drawText('Voice-to-Text PDF', {
+          x: 10,
+          y: height - 10,
+          size: 18,
+          color: rgb(0, 0, 0),
+        });
+
+        const pdfBytes = await pdfDoc.save();
+
+        const pdfUrl = URL.createObjectURL(new Blob([pdfBytes], { type: 'application/pdf' }));
+
+        // Display the PDF in a container
+        pdfContainer.innerHTML = `<embed src="${pdfUrl}" width="400" height="600" />`;
+    }
 } else {
     output.textContent = 'Speech recognition is not supported in this browser.';
 }
